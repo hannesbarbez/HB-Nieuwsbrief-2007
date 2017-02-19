56322080f8f0404e8c05e0adad08a4dd27905db5
Hannes Barbez <hannesbarbez@hotmail.com>
2017-02-19 22:19:55 +0200

    Initial Commit

---
+++ HB_NieuwsbriefDatabaseConnection/DataSource.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace HB_NieuwsbriefDatabaseConnection
+{
+    public class DataSource
+    {
+        private const string ERROR_CONNECTION = "ERROR_CONNECTION";
+        private string connectionProperties, row, table;
+
+        private MySqlConnection connection;
+        private MySqlCommand command;
+
+        public DataSource(string server, string database, string user, string password, string row, string table)
+        {
+            connectionProperties = "SERVER=" + server + ";DATABASE=" + database + ";UID=" + user + ";PASSWORD=" + password + ";";
+            this.row = row;
+            this.table = table;
+        }
+
+        private string Query(string query)
+        {
+            //initial declaration is an errormessage in case response doesn't get filled with valuable data
+            string receipients = ERROR_CONNECTION;
+            try
+            {
+                command.CommandText = query;
+                connection.Open();
+                MySqlDataReader reader = command.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+                receipients = "";
+                foreach (DataRow row in table.Rows)
+                {
+                    object[] cells = row.ItemArray; //copy cells to receipients because all contain e-mail addresses.
+                    foreach (object cell in cells) receipients += cell.ToString() + Environment.NewLine;
+                }
+                connection.Close();
+                return receipients;
+            }
+            catch
+            {
+                return receipients;
+            }
+        }
+
+        private bool MakeConnection()
+        {
+            try
+            {
+                connection = new MySqlConnection(connectionProperties);
+                command = connection.CreateCommand();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        private string[] StringToArray(string s)
+        {
+            if (!s.Equals(ERROR_CONNECTION))
+            {
+                return s.Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            }
+            else return null;
+        }
+
+        public string[] GetRecipients()
+        {
+            MakeConnection();
+            string s = Query("SELECT " + this.row + " mail FROM " + this.table);
+            return StringToArray(s.ToLower());
+        }
+    }
+}
---
+++ HB_NieuwsbriefDatabaseConnection/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HB_NieuwsbriefDatabaseConnection")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hannes Barbez")]
+[assembly: AssemblyProduct("HB_NieuwsbriefDatabaseConnection")]
+[assembly: AssemblyCopyright("©2009 Hannes Barbez")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ecfc40af-201f-4526-9c4c-dde9353ae4b8")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
